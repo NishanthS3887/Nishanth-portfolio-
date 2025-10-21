31b0ed33875003bfc6c8a0cd6ea972c954dde3db
NishanthS3887 <nishanths3887@gmail.com>
2025-10-21 13:45:37 +0530

    Professional portfolio design

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Simple smooth scrolling or animations can go here
 console.log("Portfolio loaded!");
+const links =document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    const target = document.querySelector(link.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
